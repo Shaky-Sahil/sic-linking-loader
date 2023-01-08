0a8d9df72113a954c1a0874b8632fdce576f7e7f
Sahil Shaikh <sahilpk81@gmail.com>
2023-01-08 15:36:54 +0530

    cleaned up code

--- main.c
+++ main.c
@@ -1,95 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-FILE *iptr,*outptr;
-int progaddr,progaddrcopy;
+
+FILE * iptr, * outptr;
+int progaddr, progaddrcopy;
 char record[100];
 char starting_address[100];
 int recordpointer;
 void writetooutputfile();
 int modification_locations[10];
 int requires_modification();
-int main()
-{
-    char temp[100];
-    iptr = fopen("inputprogram.txt","r");
-    outptr = fopen("outputfile.txt","w");
-    if(iptr==NULL)
-    {
-        printf("file not open");
+int main() {
+  char temp[100];
+  iptr = fopen("reloaderinput.txt", "r");
+  outptr = fopen("outputfile.txt", "w");
+  if (iptr == NULL) {
+    printf("file not open");
+  }
+  printf("Enter the program address");
+  scanf("%x", & progaddr);
+  progaddrcopy = progaddr;
+  fgets(record, sizeof(record) / sizeof(char), iptr);
+  int i = 0;
+  while (record[0] != 'E') {
+    if (record[0] == 'M') {
+      strncpy(temp, record + (2), 6);
+      modification_locations[i] = strtol(temp, NULL, 16) + progaddr;
+      i++;
     }
-    printf("Enter the starting address");
-    //scanf("%s",temp);
-    //progaddr = strtol(temp,NULL,16);
-    scanf("%x",&progaddr);
-    printf("the progaddr is:%x",progaddr);
-    progaddrcopy = progaddr;
-    fgets(record,sizeof(record)/sizeof(char),iptr);
-    int i=0;
-    while(record[0]!='E')
-    {
-        if(record[0]=='M')
-        {
-            strncpy(temp,record+(2),6);
-            modification_locations[i] = strtol(temp,NULL,16)+progaddr;
-            i++;
-        }
-        fgets(record,sizeof(record)/sizeof(char),iptr);
+    fgets(record, sizeof(record) / sizeof(char), iptr);
+  }
+  fseek(iptr, 0, SEEK_SET);
+  fgets(record, sizeof(record) / sizeof(char), iptr);
+  while (record[0] != 'E') {
+    if (record[0] == 'T') {
+      strncpy(starting_address, record + (2), 6);
+      recordpointer = 12;
+      writetooutputfile();
     }
-    fseek(iptr,0,SEEK_SET);
-    fgets(record,sizeof(record)/sizeof(char),iptr);
-    while(record[0]!='E')
-    {
-        if(record[0]=='T')
-        {
-            strncpy(starting_address,record+(2),6);
-            recordpointer=12;
-            writetooutputfile();
-        }
-        fgets(record,sizeof(record)/sizeof(char),iptr);
-    }
-    fclose(iptr);
-    fclose(outptr);
-    return 0;
+    fgets(record, sizeof(record) / sizeof(char), iptr);
+  }
+  fclose(iptr);
+  fclose(outptr);
+  printf("Relocating loader has executed successfully\nopen outputfile.txt to see the output");
+  return 0;
 }
 
-void writetooutputfile()
-{
-    char extendedobjectcode[10];
-    char objectcode[7];
-    while(record[recordpointer]!='\0'){
-    fprintf(outptr,"%lx\t",progaddr+strtol(starting_address,NULL,16));
-    if(requires_modification()==1)
-    {
-        strncpy(extendedobjectcode,record+(recordpointer),8);
-        fprintf(outptr,"%lx\n",strtol(extendedobjectcode,NULL,16)+progaddrcopy);
-        recordpointer +=9;
-        progaddr+=4;
+void writetooutputfile() {
+  char extendedobjectcode[10];
+  char objectcode[7];
+  while (record[recordpointer] != '\0') {
+    fprintf(outptr, "%lx\t", progaddr + strtol(starting_address, NULL, 16));
+    if (requires_modification() == 1) {
+      strncpy(extendedobjectcode, record + (recordpointer), 8);
+      fprintf(outptr, "%lx\n", strtol(extendedobjectcode, NULL, 16) + progaddrcopy);
+      recordpointer += 9;
+      progaddr += 4;
+    } else {
+      strncpy(objectcode, record + (recordpointer), 6);
+      fprintf(outptr, "%s\n", objectcode);
+      recordpointer += 7;
+      progaddr += 3;
     }
-    else{
-    strncpy(objectcode,record+(recordpointer),6);
-    fprintf(outptr,"%s\n",objectcode);
-    recordpointer +=7;
-    progaddr+=3;
+    if (record[recordpointer] == '^') {
+      recordpointer++;
     }
-    if(record[recordpointer]=='^')
-    {
-        recordpointer++;
-    }
-    }
+  }
 
 }
-int requires_modification()
-{
-    int i=0;
-    while(i<sizeof(modification_locations)/sizeof(int))
-    {
-        if(progaddr+strtol(starting_address,NULL,16)+1==modification_locations[i])
-        {
-            printf("modification required");
-            return 1;
-        }
-        i++;
+int requires_modification() {
+  int i = 0;
+  while (i < sizeof(modification_locations) / sizeof(int)) {
+    if (progaddr + strtol(starting_address, NULL, 16) + 1 == modification_locations[i]) {
+      return 1;
     }
-    return 0;
+    i++;
+  }
+  return 0;
 }
